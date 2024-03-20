96388734189a8f79fd553c1253f1ac9c7555f851
LeeDongWon <ldw9981@gmail.com>
2024-03-21 04:57:36 +0900

    add

--- CharCalculator/CharCalculator.cpp
+++ CharCalculator/CharCalculator.cpp
@@ -6,69 +6,34 @@
 
 #define LENGTH 10
 
-int main()
+
+char atoc(char c)
 {
-    char Num1[LENGTH] = { '8','9','8' };
-    char Num2[LENGTH] = { '8','9','8' };
-    char Res[LENGTH] = { 0 };
-
-    int result, carry;
-    for (char i = 0; i < LENGTH - 1; i++)
-    {
-        char Value1[2] = { Num1[i] , 0 };
-        char Value2[2] = { Num2[i] , 0 };
-        int n1 = atoi(Value1);
-        int n2 = atoi(Value2);
-        result = n1 + n2;
-        carry = result % 10;
-    }
-
+    if(c == '\0') 
+        return 0; // null문자면 0을 반환
+   
+	return c - '0';
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
-// CharCalculator.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <iostream>
-#include <stdlib.h>
-
-#define LENGTH 10
+char ctoa(char c)
+{
+	return c + '0';
+}
 
 int main()
 {
-    char Num1[LENGTH] = { '8','9','8' };
-    char Num2[LENGTH] = { '8','9','8' };
-    char Res[LENGTH] = { 0 };
+    char ArrayNum1[LENGTH] = { '1','4','3' };
+    char ArrayNum2[LENGTH] = { '8','9','8' };
+    char ArraySum[LENGTH] = { 0 };
 
-    int result, carry;
+    char test = atoc('\0');
+
+    char remainder=0,quotient=0,sum=0;
     for (char i = 0; i < LENGTH - 1; i++)
     {
-        char Value1[2] = { Num1[i] , 0 };
-        char Value2[2] = { Num2[i] , 0 };
-        int n1 = atoi(Value1);
-        int n2 = atoi(Value2);
-        result = n1 + n2;
-        carry = result % 10;
+        sum = atoc(ArrayNum1[i]) + atoc(ArrayNum2[i]) + quotient; // 덧셈
+        quotient = sum / 10;    // 몫
+        remainder = sum % 10;   // 나머지
+        ArraySum[i] = ctoa(remainder);  // 나머지를 배열에 저장
     }
-
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
