bc043e2a642177a21a98fc47762a22e0f30a8d30
LeeDongWon <ldw9981@gmail.com>
2024-04-16 06:40:11 +0900

    양방형 반복자 설명

--- DoubleLinkedList/List.h
+++ DoubleLinkedList/List.h
@@ -105,6 +105,15 @@ public:
 	{
 		return Iterator(nullptr);
 	}
-	
+	/*
+	RIterator RBegin()
+	{
+		return RIterator(m_pTail);   // ++ดย  pCurr = pCurr->pPrev;
+	}
+	RIterator REnd()
+	{
+		return RIterator(nullptr);
+	}
+	*/	
 };
 
